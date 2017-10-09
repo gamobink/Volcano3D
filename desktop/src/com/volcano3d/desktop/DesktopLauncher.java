0c6497f0af479903abc0b49f5c9b74a6455a473e
houseofbits <kpudzens@gmail.com>
2017-10-09 20:42:17 +0300

    x

---
+++ desktop/src/com/volcano3d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.volcano3d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.volcano3d.Volcano3D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Volcano3D(), config);
+	}
+}
