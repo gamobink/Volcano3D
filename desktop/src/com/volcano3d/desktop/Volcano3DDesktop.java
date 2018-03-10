dc125d2626cc7d70121fa2786559756269f3c41c
houseofbits <kpudzens@gmail.com>
2018-03-10 10:27:27 +0200

    k

--- desktop/src/com/volcano3d/desktop/Volcano3DDesktop.java
+++ desktop/src/com/volcano3d/desktop/Volcano3DDesktop.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.volcano3d.Volcano3D;
 
-public class DesktopLauncher {
+public class Volcano3DDesktop {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
@@ -18,7 +18,7 @@ public class DesktopLauncher {
 		config.vSyncEnabled = true;//false; // Setting to false disables vertical sync
 		
 		//config.fullscreen = true;
-		
+
 		new LwjglApplication(new Volcano3D(), config);
 	}
 }
